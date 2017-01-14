e2f0bf8699e8eed7a532ec5d13c5f8aafb718949
pengkobe <yipeng.info@gmail.com>
2017-01-14 17:14:55 +0800

    refactor nav with md-menu and fix md-icon

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,12 +8,8 @@ import { ActivatedRoute } from '@angular/router';
   template: `
     <h1>About</h1>
     <div>
-      For hot module reloading run
-      <pre>npm run start:hmr</pre>
-    </div>
-    <div>
       <h3>
-        patrick@AngularClass.com
+        yipeng.info@gmail.com
       </h3>
     </div>
     <pre>this.localState = {{ localState | json }}</pre>
