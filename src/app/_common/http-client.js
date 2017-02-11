4889aa4db904f433c87f4e40beb66edc52526273
pengkobe <yipeng.info@gmail.com>
2017-02-11 18:33:10 +0800

    add http-client.js

---
+++ src/app/_common/http-client.js
@@ -0,0 +1,41 @@
+/**
+ * 参考自:http://stackoverflow.com/questions/34464108/angular2-set-headers-for-every-request
+ */
+
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { AppState } from '../app.service';
+
+@Injectable()
+export class HttpClient {
+
+    constructor(private http: Http) {}
+
+    createAuthorizationHeader(headers: Headers, appstate: AppState) {
+        var token = appstate.token;
+        if (!token) {
+            alert("请先登录！");
+            return;
+        } else {
+            headers.append('Authorization', 'dsd ' + token);
+        }
+    }
+
+    get(url, headers) {
+        if (!headers) {
+            headers = new Headers();
+        }
+        this.createAuthorizationHeader(headers);
+        return this.http.get(url, {
+            headers: headers
+        });
+    }
+
+    post(url, data) {
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+        return this.http.post(url, data, {
+            headers: headers
+        });
+    }
+}
