a6f72a2c966f044db31d4da0e9b5b375219ba07d
pengkobe <yipeng.info@gmail.com>
2017-02-14 09:32:53 +0800

    optimizing tasks

--- server/crawers/tasks/barretlee.com.js
+++ server/crawers/tasks/barretlee.com.js
@@ -14,77 +14,77 @@ const Blognews_2 = require('../../models/blognews');
 const page = 'http://www.barretlee.com/entry/';
 
 module.exports = async function runTask(resolve, reject) {
-        superagent.get(page).end(async function (err, sres) {
+    superagent.get(page).end(async function (err, sres) {
+        // 常规的错误处理
+        if (err) {
+            console.log('get barretlee.com err!\n', err);
+            reject(err);
+            return next(err);
+        }
 
-            // 获取最新博文时间
-            let blogmodel = await Blogger.find({ name: 'barretlee' })
-                .exec().catch(err => {
-                    utils.logger.error(err);
-                    this.throw(500, '内部错误')
+        // 获取最新博文时间
+        let blogmodel = await Blogger.find({ name: 'barretlee' })
+            .exec().catch(err => {
+                utils.logger.error(err);
+                this.throw(500, '内部错误')
+            });
+
+        if (!blogmodel.length || blogmodel.length == 0) {
+            reject("no blogger!");
+            return next(err);
+        } else {
+            //console.log('barretlee.lastUpdateTime:', new Date(date) > blogmodel[0].lastUpdateTime);
+        }
+
+        let blogger = blogmodel[0];
+
+        // 提取作者博文链接，注意去重
+        let $ = cheerio.load(sres.text);
+        let newsArray = [];
+        $("ul[itemscope]").find("a").each(function (i, ele) {
+            let title = $(ele).text();
+            // 报错无返回
+            if (title == "更多文章...") {
+                return;
+            }
+            console.log('"ul[itemscope] > title:', title);
+            let rawLink = $(ele).attr('href');
+            let link = rawLink.indexOf("http") == -1 ? ('http://www.barretlee.com' + rawLink) : rawLink;
+            let patt = /\d{4}\/\d{1,2}\/\d{0,2}/g;
+
+            let date = patt.exec(link)[0];
+
+            // 时间对比 new Date("2016-11-01")
+            if (!blogger.lastUpdateTime || new Date(date) > new Date(blogger.lastUpdateTime)) {
+                newsArray.push({
+                    from: blogger._id,
+                    pullTime: new Date(),
+                    publishTime: new Date(date),
+                    title: title,
+                    link: link,
+                    date: date
                 });
-
-            // 常规的错误处理
+            }
+        });
+        console.log('  存入数据库  前!\n', err);
+        // 存入数据库
+        Blognews_2.create(newsArray, function (err) {
             if (err) {
-                console.log('get barretlee.com err!\n', err);
-                reject(err);
+                reject("store article err！");
                 return next(err);
             }
-
-            // 提取作者博文链接，注意去重
-            let $ = cheerio.load(sres.text);
-            let newsArray = [];
-
-            $("ul[itemscope]").find("a").each(function (i, ele) {
-                let title = $(ele).text();
-                // 报错无返回
-                if(title == "更多文章..."){
-                    return;
-                }
-                console.log('"ul[itemscope] > title:', title);
-                let rawLink =  $(ele).attr('href');
-                let link = rawLink.indexOf("http") == -1 ? ('http://www.barretlee.com' + rawLink):rawLink ;
-                let patt = /\d{4}\/\d{1,2}\/\d{0,2}/g;
-
-                let date = patt.exec(link)[0];
-
-                if (!blogmodel.length || blogmodel.length == 0) {
-                    reject("no blogger!");
-                    return next(err);
-                } else {
-                     //console.log('barretlee.lastUpdateTime:', new Date(date) > blogmodel[0].lastUpdateTime);
-                }
-
-                // 时间对比 new Date("2016-11-01")
-                if (!blogmodel[0].lastUpdateTime  ||  new Date(date) > new Date(blogmodel[0].lastUpdateTime)) {
-                    newsArray.push({
-                        from: blogmodel[0]._id,
-                        pullTime: new Date(),
-                        publishTime: new Date(date),
-                        title: title,
-                        link: link,
-                        date: date
-                    });
-                }
-            });
-            console.log('  存入数据库  前!\n', err);
-            // 存入数据库
-            Blognews_2.create(newsArray, function (err) {
-                if (err) {
-                    reject("store article err！");
-                    return next(err);
-                }
-                var docs = Array.prototype.slice.call(arguments, 1);
-                resolve(docs);
-            });
-
-            // 更新最后拉取时间
-            blogmodel[0].lastUpdateTime = new Date();
-            await Blogger.update({ _id: blogmodel[0]._id }, blogmodel[0])
-                .exec().catch(err => {
-                    utils.logger.error(err);
-                    this.throw(500, 'Blogger.update错误');
-                });
+            var docs = Array.prototype.slice.call(arguments, 1);
+            resolve(docs);
         });
-    }
+
+        // 更新最后拉取时间
+        blogger.lastUpdateTime = new Date();
+        await Blogger.update({ _id: blogger._id }, blogger)
+            .exec().catch(err => {
+                utils.logger.error(err);
+                this.throw(500, 'Blogger.update错误');
+            });
+    });
+}
 
 
--- server/crawers/tasks/yinwang.org.js
+++ server/crawers/tasks/yinwang.org.js
@@ -14,80 +14,80 @@ const Blognews_2 = require('../../models/blognews');
 const page = 'http://yinwang.org';
 
 
-module.exports =  async function runTask(resolve, reject) {
-        superagent.get(page).end(async function (err, sres) {
+module.exports = async function runTask(resolve, reject) {
+    superagent.get(page).end(async function (err, sres) {
+        // 常规的错误处理
+        if (err) {
+            console.log('get yinwang.org err!\n', err);
+            reject(err);
+            return next(err);
+        }
 
-            // 获取最新博文时间
-            let blogmodel = await Blogger.find({ name: 'yinwang' })
-                .exec().catch(err => {
-                    utils.logger.error(err);
-                    this.throw(500, '内部错误')
+        // 获取最新博文时间
+        let blogmodel = await Blogger.find({ name: 'yinwang' })
+            .exec().catch(err => {
+                utils.logger.error(err);
+                this.throw(500, '内部错误')
+            });
+
+        if (!blogmodel.length || blogmodel.length == 0) {
+            reject("no blogger!");
+            return next(err);
+        } else {
+            //console.log('blogmodel.lastUpdateTime:', new Date(date) > blogmodel[0].lastUpdateTime);
+        }
+        let blogger =  blogmodel[0];
+
+        // 提取作者博文链接，注意去重
+        let $ = cheerio.load(sres.text);
+        let newsArray = [];
+        // 获取欧文
+        $('.list-group-item').each(function (i, ele) {
+            let title = $('a', ele).text();
+            let link = $('a', ele).attr('href');
+            let patt = /\d{4}\/\d{1,2}\/\d{0,2}/g;
+            let date = patt.exec(link)[0];
+
+            // for test
+            // let link = "http://yinwang.org/blog-cn/2016/10/12/compiler-bug";
+            // let patt = /blog-cn\/(\d{4}\/\d{1,2}\/\d{0,2})/g;
+            // let date = patt.exec(link)[1];
+
+            // 时间对比 new Date("2016-11-01")
+            if (!blogger.lastUpdateTime || new Date(date) > new Date(blogger.lastUpdateTime)) {
+                newsArray.push({
+                    from: blogger._id,
+                    pullTime: new Date(),
+                    publishTime: new Date(date),
+                    title: title,
+                    link: link,
+                    date: date
                 });
+            }
+        });
 
-            // 常规的错误处理
+        // 存入数据库
+        Blognews_2.create(newsArray, function (err) {
             if (err) {
-                console.log('get yinwang.org err!\n', err);
-                reject(err);
+                reject("store article err！");
                 return next(err);
             }
-
-            // 提取作者博文链接，注意去重
-            let $ = cheerio.load(sres.text);
-            let newsArray = [];
-            // 获取欧文
-            $('.list-group-item').each(function (i, ele) {
-                let title = $('a', ele).text();
-                let link = $('a', ele).attr('href');
-                let patt = /\d{4}\/\d{1,2}\/\d{0,2}/g;
-                let date = patt.exec(link)[0];
-
-                // for test
-                // let link = "http://yinwang.org/blog-cn/2016/10/12/compiler-bug";
-                // let patt = /blog-cn\/(\d{4}\/\d{1,2}\/\d{0,2})/g;
-                // let date = patt.exec(link)[1];
-
-                if (!blogmodel.length || blogmodel.length == 0) {
-                    reject("no blogger!");
-                    return next(err);
-                } else {
-                     //console.log('blogmodel.lastUpdateTime:', new Date(date) > blogmodel[0].lastUpdateTime);
-                }
-
-                // 时间对比 new Date("2016-11-01")
-               if (!blogmodel[0].lastUpdateTime  ||  new Date(date) > new Date(blogmodel[0].lastUpdateTime)) {
-                    newsArray.push({
-                        from: blogmodel[0]._id,
-                        pullTime: new Date(),
-                        publishTime: new Date(date),
-                        title: title,
-                        link: link,
-                        date: date
-                    });
-                }
-            });
-
-            // 存入数据库
-            Blognews_2.create(newsArray, function (err) {
-                if (err) {
-                    reject("store article err！");
-                    return next(err);
-                }
-                var docs = Array.prototype.slice.call(arguments, 1);
-                resolve(docs);
-            });
-
-            // 更新最后拉取时间
-            blogmodel[0].lastUpdateTime = new Date();
-            await Blogger.update({ _id: blogmodel[0]._id }, blogmodel[0])
-                .exec().catch(err => {
-                    utils.logger.error(err);
-                    this.throw(500, 'Blogger.update错误');
-                });
-
-            // ===== 用于抓取内容（TODO）
-            // evtProxyObj.emit('yiwang_indexpage_finished', 'get yinwang.org successful');
-            // =====
+            var docs = Array.prototype.slice.call(arguments, 1);
+            resolve(docs);
         });
-    }
+
+        // 更新最后拉取时间
+        blogger.lastUpdateTime = new Date();
+        await Blogger.update({ _id: blogger._id }, blogger)
+            .exec().catch(err => {
+                utils.logger.error(err);
+                this.throw(500, 'Blogger.update错误');
+            });
+
+        // ===== 用于抓取内容（TODO）
+        // evtProxyObj.emit('yiwang_indexpage_finished', 'get yinwang.org successful');
+        // =====
+    });
+}
 
 
